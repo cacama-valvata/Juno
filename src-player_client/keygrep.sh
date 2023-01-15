68d458657375320ef86fbf6d87070bd5efb388cd
Casey Colley <code@caseycolley.com>
2023-01-15 14:16:21 -0800

    recevies public key (already b64-encoded), greps it in a file

--- src-player_client/keygrep.sh
+++ src-player_client/keygrep.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 #test w/ ./decode_key.sh <base64_encoded_key>
-key=$1
-decoded_key=$(echo $key | base64 --decode)
-echo $decoded_key
+# <base64_encoded_key> refers to the part of the public key without `ssh-rsa` at beginning or `user@hostname` at end
+
+# define the path to the authorized_keys file
+AUTH_KEYS_FILE="/home/self_shell/.ssh/authorized_keys.bak"
+
+# define the public key to search for
+PUB_KEY=$1
+
+# search the file for the key and print the entire line
+grep "$PUBKEY" "$AUTH_KEYS_FILE"
