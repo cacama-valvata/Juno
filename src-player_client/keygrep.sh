229060f8916e5a56fdcf91273fe8da30942e8958
Casey Colley <code@caseycolley.com>
2023-01-18 12:24:44 -0800

    tweak to make easier to test

--- src-player_client/keygrep.sh
+++ src-player_client/keygrep.sh
@@ -4,7 +4,8 @@
 # <base64_encoded_key> refers to the part of the public key without `ssh-rsa` at beginning or `user@hostname` at end
 
 # define the path to the authorized_keys file
-AUTH_KEYS_FILE="/home/self_shell/.ssh/authorized_keys.bak"
+# !!! addition of $2 %u is not for production design, only for ease of testing
+AUTH_KEYS_FILE="/home/$2/.ssh/authorized_keys.bak"
 
 # define the public key to search for
 PUB_KEY=$1
@@ -19,7 +20,7 @@ grep "$PUBKEY" "$AUTH_KEYS_FILE"
 # An openssh server set up locally.
 # sshd_config:
 # ```
-# AuthorizedKeysCommand /bin/bash -c "/home/casey/Repos/Juno/src-player_client/keygrep.sh %k"
+# AuthorizedKeysCommand /bin/bash -c "/home/casey/Repos/Juno/src-player_client/keygrep.sh %k %u"
 # AuthorizedKeysCommandUser root
 # ```
 # A target ssh user `self_shell` with 'shellserver.out' shell
