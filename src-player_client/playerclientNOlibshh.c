63984e439b689049869e61063634feb38af9a0e6
CarterM00 <macnabc@oregonstate.edu>
2023-01-18 22:31:58 -0800

    auth cleanup and playerclient start
    playerclient work started, still very WIP

--- src-player_client/playerclientNOlibshh.c
+++ src-player_client/playerclientNOlibshh.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "wireguard.h"
 
@@ -8,12 +9,6 @@
 //init the connection and allow user to mess around in shell
 void connectclient(int argc, char *argv[])
 {
-    if (argc < 4) 
-    {
-        printf("Usage: %s [user] [hostname] [shell]\n", argv[0]);
-        return;
-    }
-
     int pipefd[2];
     pid_t pid;
     char buf[BUF_SIZE];
@@ -55,7 +50,7 @@ void connectclient(int argc, char *argv[])
     return;
 }
 
-void wgkeygen(){
+void wgkeygen(char *pubkeyz, char *privkeyz){
 
 
   // generate public key (if not already existing)
@@ -69,16 +64,103 @@ void wgkeygen(){
 
   //generate and maybe send to file
   printf("here is my public key %s\n", pubkeys);
+  memcpy(pubkeyz, pubkeys, 32);
+  memcpy(privkeyz, privkey, 32);
+
 
   return;
 
 }
 
 
+//checking for key file basic, needs to be modified if reading wg file
+int check_key(char *pubkey, char *privkey) {
+    FILE *file;
+   
+    // read file
+    file = fopen("keypair.txt", "r");
+    if (file == NULL) {
+        return 0;
+    }
 
+    // grab first two lines
+    fgets(pubkey, 32, file);
+    fgets(privkey, 32, file);
+
+    fclose(file);  
+
+    //cut off newline and save
+    pubkey[strcspn(pubkey, "\n")] = 0;
+    privkey[strcspn(privkey, "\n")] = 0;
+    return((strlen(pubkey) > 30) && (strlen(privkey) > 30));
+}
+
+void process_config(const char* file_name, const char* search_str, const char* replace_str) {
+    FILE* fp = fopen(file_name, "r+");
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return;
+    }
+
+    // Get the length of the search string
+    int search_len = strlen(search_str);
+
+    // Allocate memory for the buffer
+    char* buffer = (char*) malloc(search_len);
+
+    // Read the file one character at a time
+    int c;
+    while ((c = fgetc(fp)) != EOF) {
+        // Check if the current character is the same as the first character of the search string
+        if (c == search_str[0]) {
+            // Read the next search_len characters into the buffer
+            fread(buffer, 1, search_len, fp);
+
+            // Check if the buffer matches the search string
+            if (strncmp(buffer, search_str, search_len) == 0) {
+                // Move the file pointer back to the beginning of the search string
+                fseek(fp, -search_len, SEEK_CUR);
+
+                // Write the replace string
+                fwrite(replace_str, 1, strlen(replace_str), fp);
+
+                // Move the file pointer to the end of the replace string
+                fseek(fp, strlen(replace_str) - search_len, SEEK_CUR);
+            } else {
+                // Move the file pointer back to the beginning of the buffer
+                fseek(fp, -search_len, SEEK_CUR);
+            }
+        }
+    }
+
+    // Close the file and free the buffer
+    fclose(fp);
+    free(buffer);
+
+    // attempt to bring up wiregaurd daemon 
+}
+
+
+
+// Casey I know this is NOT secure just getting the logic ready :)
 int main(int argc, char* argv[])
  {
     char user_input[4096];
+    char pubkey[32], privkey[32];
+    
+
+
+    if (argc < 4) 
+    {
+        printf("Usage: %s [user] [hostname] [shell]\n", argv[0]);
+        return 1;
+    }
+
+    // check for existing config, if not regen
+    if(check_key(pubkey,privkey) == 0)
+        wgkeygen(pubkey,privkey);
+
+
     
     // very simple menuing 
     while(1)
@@ -89,13 +171,15 @@ int main(int argc, char* argv[])
         if(strcmp(user_input, "connect\n") == 0 )
             connectclient(argc,argv);
         
-        
         else if(strcmp(user_input, "wgkeygen\n")  == 0 )
-            wgkeygen();
+            wgkeygen(pubkey, privkey);
 
+        else if(strcmp(user_input, "proccfg\n") == 0 )
+            process_config("file.txt", "<your private key here>", privkey);
+    
         else if(strcmp(user_input, "stop\n") == 0 )
             break;
-
+        
         else
             printf("UNKNOWN\n");
     }
--- src-player_client/smallsh_ssh_auth.c
+++ src-player_client/smallsh_ssh_auth.c
@@ -6,36 +6,42 @@
 
 
 #define BUFSIZE 256
+#define INPUTLEN 32
 
 
-int main(int argc, char *argv[])
+void read_credentials(char *username, char *password)
 {
-    //char pubkey = 'x';
-    char pubkey_decoded = 'x';
-    //char *username;
-    char *query;
-    //int count;
-    // char *ret;
-    int send = 0;
+    FILE *file;
+    file = fopen("credentials.txt", "r");
+    if (file == NULL) {
+        printf("Error opening file");
+        return;
+    }
+    fgets(username, INPUTLEN, file);
+    fgets(password, INPUTLEN, file);
+    fclose(file);
+}
+
+void execdb(char *username, char *password, char* query, char* buf)
+{
 
     int pipefd[2];
     pid_t pid;
-    char buf[BUFSIZE];
     int bytes_read; 
 
-    //grab and decode pubkey
+
 
     if (pipe(pipefd) == -1)
     {
         perror("pipe");
-        return 1;
+        return;
     }
 
     pid = fork();
     if (pid == -1)
     {
         perror("fork");
-        return 1;
+        return;
     }
 
     if (pid == 0) 
@@ -45,9 +51,7 @@ int main(int argc, char *argv[])
         dup2(pipefd[1], STDOUT_FILENO);  // redirect stdout to write end of pipe
 
         // construct SQL query to check for unique user with this public key
-        query = "SELECT COUNT(*), username FROM users WHERE pubkey = '";
-        query = strcat(query, &pubkey_decoded);
-        query = strcat(query, "';");
+
         execl("/usr/bin/mysql", "mysql", "-u", "user", "-p", "password", "-e", query, NULL);
     } 
 
@@ -65,48 +69,50 @@ int main(int argc, char *argv[])
         wait(NULL);
     }
 
-    if(send)
+
+}
+
+
+int main(int argc, char *argv[])
+{
+    //char pubkey = 'x';
+    char pubkey_decoded = 'x';
+    char *query;
+    char buf[BUFSIZE];
+    char username[INPUTLEN], password[INPUTLEN];
+    
+    //grab creds for db query
+    read_credentials(username, password);
+
+    //grab and decode pubkey
+
+    query = "SELECT COUNT(*), username FROM users WHERE pubkey = '";
+    query = strcat(query, &pubkey_decoded);
+    query = strcat(query, "';");
+
+    execdb(username, password, query, buf);
+
+    //parse buf into simple output
+
+    if(strcmp(buf,"1") == 0)
     {
+        //grab actual username 
+        query = "SELECT ALL, username FROM users WHERE pubkey = '";
+        query = strcat(query, &pubkey_decoded);
+        query = strcat(query, "';");
 
-        if (pipe(pipefd) == -1)
-        {
-            perror("pipe");
-            return 1;
-        }
+        execdb(username, password, query, buf);
 
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("fork");
-            return 1;
-        }
+        //parse buf into simple output
 
-        if (pid == 0) 
-        {  // child process
-            close(pipefd[0]);  // close unused read end
-            dup2(pipefd[1], STDOUT_FILENO);  // redirect stdout to write end of pipe
-
-            // construct SQL query to check for unique user with this public key
-            query = "SELECT, username FROM users WHERE pubkey = '";
-            query = strcat(query, &pubkey_decoded);
-            query = strcat(query, "';");
-            execl("/usr/bin/mysql", "mysql", "-u", "user", "-p", "password", "-e", query, NULL);
-        } 
-
-        else 
-        {  
-            // parent process
-            close(pipefd[1]);
-            bytes_read = read(pipefd[0], buf, BUFSIZE);
-            if (bytes_read > 0)
-            {
-                buf[bytes_read] = '\0';
-                printf("%s", buf);
-            }
-            close(pipefd[0]);
-            wait(NULL);
-        }
+        // send command to login and replace program w/ shell
+        execl("/usr/bin/", "login", buf, "ssh-key", &pubkey_decoded,  NULL);
     }
+        
+
+    else if(strcmp(buf,"0") != 0)
+        fprintf(stderr, "Error: should not be > 1");
+
 
 
     return 0;
