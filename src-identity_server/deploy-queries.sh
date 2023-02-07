c237dca91de9c2f127d00dbffa4e265cbb065745
Casey Colley <code@caseycolley.com>
2023-02-07 12:05:44 -0800

    added all sql files to deployment script

--- src-identity_server/deploy-queries.sh
+++ src-identity_server/deploy-queries.sh
@@ -1,10 +1,21 @@
 # Quick Start to set up the IAM database
 # For player client testing so far
 
-# Intended usage: `sudo ./as_root.sh`
 # If unable to execute, verify `chmod +x ./sa_root.sh`
+# Need a SQL server first? Try:
+#	sudo apt install mysql-server-8.0
 
-apt install mysql-server
+HOST='localhost'
 
-mysql -u root -proot -e "source create_tables.sql; show tables;"
+# Creating the database
+mysql -u root -h $HOST -proot -e "source create_tables.sql; show tables;"
 
+# Inserting fake users
+mysql -u root -h $HOST -proot -e "source insert_fakeusers.sql;"
+
+# Print out fake users to verification
+#	Note: won't show the full SSH keys for brevity
+mysql -u root -h $HOST -proot -e "source show_fakeusers.sql;"
+
+# To delete everything and re-deploy, run:
+#	mysql -u -h $HOST -proot -e "drop database Juno;"
