486e5dd8508e040f3e0b4bc64b3bd7fb7e391c4e
Casey Colley <code@caseycolley.com>
2023-02-07 12:10:57 -0800

    Merged from branch 'web' - SQL queries for testing of the player-client
    Squashed commit of the following:
    
    commit c237dca91de9c2f127d00dbffa4e265cbb065745
    Author: Casey Colley <code@caseycolley.com>
    Date:   Tue Feb 7 12:05:44 2023 -0800
    
        added all sql files to deployment script
    
    commit be0bd6eae7f99555a5ddb02ad450d673f32109ff
    Author: Casey Colley <code@caseycolley.com>
    Date:   Tue Feb 7 12:00:43 2023 -0800
    
        inserting and showing the fake users, as well as fake keys for SSH auth use (testing)
    
    commit a3c4bf93afcaa4ef2937e49a9b11500f3b438210
    Author: Casey Colley <code@caseycolley.com>
    Date:   Tue Feb 7 12:00:13 2023 -0800
    
        changed script
    
    commit 695fd72ffeea54104c39992efe7b28f8dc1b5d64
    Author: Casey Colley <code@caseycolley.com>
    Date:   Tue Feb 7 11:58:08 2023 -0800
    
        better table ids
    
    commit 8a91c8141ab60ac8dd77e0d55ea16591a636780b
    Author: Casey Colley <code@caseycolley.com>
    Date:   Tue Feb 7 06:21:41 2023 -0800
    
        create_tables - added full ssh keys
    
    commit 356e562b4f651a55eef35d4f2b6d537fe64272db
    Author: Casey Colley <code@caseycolley.com>
    Date:   Sun Feb 5 22:06:04 2023 -0800
    
        sql queries to set up iam database for player client testing

---
+++ src-identity_server/deploy-queries.sh
@@ -0,0 +1,21 @@
+# Quick Start to set up the IAM database
+# For player client testing so far
+
+# If unable to execute, verify `chmod +x ./sa_root.sh`
+# Need a SQL server first? Try:
+#	sudo apt install mysql-server-8.0
+
+HOST='localhost'
+
+# Creating the database
+mysql -u root -h $HOST -proot -e "source create_tables.sql; show tables;"
+
+# Inserting fake users
+mysql -u root -h $HOST -proot -e "source insert_fakeusers.sql;"
+
+# Print out fake users to verification
+#	Note: won't show the full SSH keys for brevity
+mysql -u root -h $HOST -proot -e "source show_fakeusers.sql;"
+
+# To delete everything and re-deploy, run:
+#	mysql -u -h $HOST -proot -e "drop database Juno;"
