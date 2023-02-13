a3b27362e4f0213e0ce99904ee47c23e0443abdb
Casey Colley <code@caseycolley.com>
2023-02-13 09:27:29 -0800

    fixed django secret

--- src-identity_server/JunoIAM/settings.py
+++ src-identity_server/JunoIAM/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9owlh8w)4-(zr8bm+^c##i=b0)7=!2d!i_+_^vccq(96-^+y4h'
+with open ("JunoIAM/django.secret", mode='r') as dj:
+    SECRET_KEY = dj.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
