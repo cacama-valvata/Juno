2748dae570f161d3537d8070ff7a3d6fda7078b8
Casey Colley <code@caseycolley.com>
2023-02-12 16:00:11 -0800

    updated pip requirements, added routing for user auth, added routing for 'apps' = our pages

--- src-identity_server/JunoIAM/settings.py
+++ src-identity_server/JunoIAM/settings.py
@@ -73,10 +73,16 @@ WSGI_APPLICATION = 'JunoIAM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+with open ("JunoIAM/mysql.creds", mode='r') as msc:
+    mysqlcreds = msc.readlines()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Juno',
+        'USER': mysqlcreds[0].strip(),
+        'PASSWORD': mysqlcreds[1].strip(),
+        'HOST': mysqlcreds[2].strip(),
     }
 }
 
--- src-identity_server/JunoIAM/urls.py
+++ src-identity_server/JunoIAM/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path ('', include ('leaderboard.urls')),
+    path ('admin/', admin.site.urls),
+    path ('profile/', include ('account.urls')),
+    path ('profile/', include ("django.contrib.auth.urls")),
+    path ('games/', include('games.urls')),
+    path ('leaderboard/', include('leaderboard.urls')),
 ]
