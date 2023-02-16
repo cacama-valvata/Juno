7025a979c7be95e03429bcd1925bcb1c36330d17
CarterM00 <macnabc@oregonstate.edu>
2023-02-15 17:22:39 -0800

    edit touchups

--- src-player_client/playershell/main.c
+++ src-player_client/playershell/main.c
@@ -69,7 +69,7 @@ int main (int argc, char* argv[])
 
     
     for (int i = 0; i < argc; i++) {
-        printf("Argument %d: %s\n", i, argv[i]);
+        printf("[1]Argumentz %d: %s\n", i, argv[i]);
     }
 
     // non-interactive commands
