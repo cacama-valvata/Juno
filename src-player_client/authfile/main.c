a7f5bd1a869598f066935790c31d4dc9dbfa6964
CarterM00 <macnabc@oregonstate.edu>
2023-02-15 18:04:40 -0800

    NO MORE COMMENTS

--- src-player_client/authfile/main.c
+++ src-player_client/authfile/main.c
@@ -20,9 +20,9 @@ int main(int argc, char *argv[])
 
     strcpy(pubkey_decoded,"AAAAB3NzaC1yc2EAAAADAQABAAABgQDV6pvDAafe6Emu305YWzxuEWgWq6ypPpMftmHVtu5al9+VzLSFFph2J5hqUL06M8W7+5b1a+UXxdns7hUIHYEh2WPxJkPf4xPXyEG3bXeoTXyt4YZ3i+PagmvcYLg4pwUJl2y5A1/ipamdZTqoPNtycIDt62+pPEjBUL7QnE3YPIP6Fzh76SQcPHS46t7MDzSahBkAvaTMErCfYfyFgIrWgcWbVY+pM+JPyfNpAQs/XkYxXtctzieasPqkOlLc/Y2XKeB6SiXZiiOuu8zHTNfuAd1OO1yZ0Bp0+Bph/ZlsLmrPVsEzgw0yzOn+Mz9zMng1flQHQqxA2kLzI8lk8/HbIf9byKzIJebRdsOBJEnY+/xyEO8wxx5bgs3MshtDe9lNyKraaEgS/+c87Q4p1v6tUZdXSRPTSjyeczYxRVAMFU0NVo+WZvnG0KIHx5wMUX46tBiQFlW+gdhs1EC1wdkcC8K9zxZuK9685duhRbZp0DMgWsSC6wJi8mAUhYAT8Es=");
 
-    for (int i = 0; i < argc; i++) {
-        fprintf(stderr,"[0]Argument %d: %s\n", i, argv[i]);
-    }
+    // for (int i = 0; i < argc; i++) {
+    //     fprintf(stderr,"[0]Argument %d: %s\n", i, argv[i]);
+    // }
     
     //grab creds for db query
     if(read_credentials(username, password) == 1)
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
 
     freopen("/dev/tty", "w", stdout);
 
-    printf("Result: %s\n", buf);
+    //printf("Result: %s\n", buf);
 
     //parse buf into simple output
 
