652be5b41e559deb054770c91c7909aab600d651
Arian Ghorbani <arighorbani@gmail.com>
2023-02-17 16:22:34 -0800

    commit for iteration -1 demo

--- src-env_architect/test.rb
+++ src-env_architect/test.rb
@@ -1,2 +1,26 @@
 #!/usr/bin/ruby
-print "Hello world!\n"
+require 'json'
+
+#read and parse settings.json
+file = File.read('./settings.json')
+document = JSON.parse(file)
+
+#TODO: support for multiple VMs being asked for to support an entire network
+
+#output the script w/ functions
+#STEPS: Look for all listed scripts, throw all their functions into a wrapper script, and output that wrapper script with a text file containing the name of the machine image
+
+#TODO: this should be scriptwrapper.sh
+wrapper = File.open("scriptwrapper.txt", 'w')
+for x in document['vuln'] do
+    wrapper << x[1] + "\n"
+end
+wrapper.close
+
+#Now, output textfile with name of machine image
+image = File.open("image.txt", 'w')
+
+image << document['image'] + "\n"
+
+image.close
+#print document
