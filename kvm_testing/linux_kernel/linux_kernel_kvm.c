ab684261416e439dfc34023cb21a5409451f5759
TheLastMarfist <alexmarx177@gmail.com>
2023-02-26 19:08:09 -0800

    commented code and added makefile

--- kvm_testing/linux_kernel/linux_kernel_kvm.c
+++ kvm_testing/linux_kernel/linux_kernel_kvm.c
@@ -14,7 +14,7 @@
 #include <errno.h>
 #include <unistd.h>
 
-
+// represents a vm, with each fd, and the mem pointer
 struct vm {
 	int kvm_fd;
 	int vm_fd;
@@ -22,6 +22,7 @@ struct vm {
 	void *mem;
 };
 
+// prints out the error when a ioctl call fails
 static int vm_error(struct vm *v, const char *fmt, ...) {
 	va_list args;
 	va_start(args, fmt);
@@ -31,6 +32,7 @@ static int vm_error(struct vm *v, const char *fmt, ...) {
 	return -1;
 }
 
+// inits registers for the linux kernel
 static int vm_init_regs(struct vm *v) {
 
 	//initializing registers
@@ -81,8 +83,8 @@ static int vm_init_regs(struct vm *v) {
 	}
 
 	regs.rflags = 2;
-	regs.rip = 0x100000;
-	regs.rsi = 0x10000;
+	regs.rip = 0x100000; // where the kernel code starts
+	regs.rsi = 0x10000; // where the boot params start
 
 	//set regs
 	if(ioctl(v->vcpu_fd, KVM_SET_REGS, &(regs)) < 0) {
@@ -91,6 +93,7 @@ static int vm_init_regs(struct vm *v) {
 	return 0;
 }
 
+// inits cpu id so kernel knows it is running as a vm
 static int vm_init_cpu_id(struct vm *v) {
 
 	struct {
@@ -99,9 +102,12 @@ static int vm_init_cpu_id(struct vm *v) {
 		struct kvm_cpuid_entry2 entries[100];
 	} kvm_cpuid;
 
+	// gets number of entires
 	kvm_cpuid.nent = sizeof(kvm_cpuid.entries) / sizeof(kvm_cpuid.entries[0]);
+	// gets cpuid features supported by both hardware and kvm in default config
 	ioctl(v->kvm_fd, KVM_GET_SUPPORTED_CPUID, &kvm_cpuid);
 
+	// for each entry
 	for (unsigned int i = 0; i < kvm_cpuid.nent; i++) {
 		struct kvm_cpuid_entry2 *entry = &kvm_cpuid.entries[i];
 		if(entry->function == KVM_CPUID_SIGNATURE) {
@@ -111,6 +117,7 @@ static int vm_init_cpu_id(struct vm *v) {
 			entry->edx = 0x4d; //M
 		}
 	}
+	//constructing cpuid information and setting it
 	ioctl(v->vcpu_fd, KVM_SET_CPUID2, &kvm_cpuid);
 	return 0;
 }
@@ -129,7 +136,11 @@ int vm_init(struct vm *v) {
 		return vm_error(v, "failed to create vm");
 	}
 
-	//setting TSS Addr
+	//sets up the tss (task state segment)
+	//is used by intel cpus to store info about a task namely
+	//processor register state
+	//i/o port perms
+	//previous tss link
 	if(ioctl(v->vm_fd, KVM_SET_TSS_ADDR, 0xffffd000) < 0) {
 		return vm_error(v, "failed to set tss addr");
 	}
@@ -140,12 +151,12 @@ int vm_init(struct vm *v) {
 		return vm_error(v, "failed to set identity map addr");
 	}
 
-	//creating irqchip
+	//creates an interrupt controller module
 	if(ioctl(v->vm_fd, KVM_CREATE_IRQCHIP, 0) < 0) {
 		return vm_error(v, "failed to create irq chip");
 	}
 
-	//creating in kernel device modelf for i8254 PIT
+	//creating in kernel device model for i8254 PIT (programmable interval timer)
 	struct kvm_pit_config pit = {
 		.flags = 0,
 	};
@@ -194,46 +205,64 @@ int vm_load(struct vm *v, const char *image_path) {
 		return 1;
 	}
 
+	//gets information from open file and stores it in stat stuct
 	struct stat st;
 	fstat(fd, &st);
+	//creates memory for file of size of file
 	data = mmap(0, st.st_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
 	datasz = st.st_size;
 	close(fd);
 
+	//beginning of boot params
 	struct boot_params *boot = (struct boot_params *)(((uint8_t *)v->mem) + 0x10000);
+	//where the cmdline boot parameter is
 	void *cmdline = (void *)(((uint8_t *)v->mem) + 0x20000);
+	//where the kernel code starts
 	void *kernel = (void *)(((uint8_t *)v->mem) + 0x100000);
 
 	memset(boot , 0, sizeof(struct boot_params));
+	//grabs boot params from image
 	memmove(boot, data, sizeof(struct boot_params));
+	//gets number of setup sectors
 	size_t setup_sectors = boot->hdr.setup_sects;
+	//gets size of sectors (will use this to jump to kernel code)
 	size_t setupsz = (setup_sectors + 1) * 512;
-	boot->hdr.vid_mode = 0xFFFF; //VGA
-	boot->hdr.type_of_loader = 0xFF;
-	boot->hdr.ramdisk_image = 0x0;
-	boot->hdr.ramdisk_size = 0x0;
+
+	boot->hdr.vid_mode = 0xFFFF; //sets to vga
+	boot->hdr.type_of_loader = 0xFF; //boot loader does not have assigned id
+	boot->hdr.ramdisk_image = 0x0; //32bit address of initial ramdisk - no ramdisk
+	boot->hdr.ramdisk_size = 0x0; // size of initial ramdisk - no ramdisk
+	// can_use_heap means the value in head_end_ptr is valid
+	// keep_segments in obsolete
+	// i believe teh 0x01 is setting loaded_high to one meaning the protected mode code is at 0x100000 instead of 0x10000
 	boot->hdr.loadflags |= CAN_USE_HEAP | 0x01 | KEEP_SEGMENTS;
-	boot->hdr.heap_end_ptr = 0xFE00;
-	boot->hdr.ext_loader_ver = 0x0;
-	boot->hdr.cmd_line_ptr = 0x20000;
+	boot->hdr.heap_end_ptr = 0xFE00; // offset from beginning of real-mode code to end of setup stack/head minux 0x0200
+	boot->hdr.ext_loader_ver = 0x0; // extension of type_of_loader
+	boot->hdr.cmd_line_ptr = 0x20000; //linear addr of kernel command line
 
 	memset(cmdline, 0, boot->hdr.cmdline_size);
+	//getting kernel to send output to ttyS0 (vga port 0)
 	memcpy(cmdline, "console=ttyS0", 14);
+	//move kernel code into vm memory
 	memmove(kernel, (char *)data + setupsz, datasz - setupsz);
 	return 0;
 }
 
 int vm_run(struct vm *v) {
 
+	//gets size of shared mem region kvm_run uses to communicate with userspace
 	int run_size = ioctl(v->kvm_fd, KVM_GET_VCPU_MMAP_SIZE, 0);
+	//getting ptr to kvm_run struct
 	struct kvm_run *run = mmap(0, run_size, PROT_READ | PROT_WRITE, MAP_SHARED, v->vcpu_fd, 0);
 	for (;;) {
 		//printf("Here\n");
+		//runs the vm
 		int ret = ioctl(v->vcpu_fd, KVM_RUN, 0);
 		if (ret < 0) {
 			return vm_error(v, "kvm_run failed");
 		}
 		
+		//gets exit reason
 		switch (run->exit_reason) {
 			case KVM_EXIT_IO:
 				if (run->io.port == 0x3f8 && run->io.direction == KVM_EXIT_IO_OUT) {
@@ -256,6 +285,7 @@ int vm_run(struct vm *v) {
 	}
 }
 
+//closes all fd, and unmaps vm memory
 void vm_exit(struct vm *v) {
 	close(v->kvm_fd);
 	close(v->vm_fd);
