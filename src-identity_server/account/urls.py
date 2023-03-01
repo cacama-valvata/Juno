4ca129f389d6841d8e139b18586ec059f203d32f
Casey Colley <code@caseycolley.com>
2023-03-01 12:12:32 -0800

    devices page

--- src-identity_server/account/urls.py
+++ src-identity_server/account/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from . import views
-from .views import SignUpView
+from .views import *
 from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path ('', TemplateView.as_view(template_name='profile/home.html'), name='home'),
     path ('signup/', SignUpView.as_view(), name='signup'),
-    path ('devices/', TemplateView.as_view(template_name='profile/devices.html'), name='devices'),
+    path ('devices/', DevicesView, name='devices'),
+    path ('devices/add/', AddDevice, name='devies-add')
 ]
