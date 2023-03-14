a85ce9a5bd41a64ce289e4fbc7b93346a8d3a8fe
CarterM00 <macnabc@oregonstate.edu>
2023-03-13 18:08:12 -0700

    progerrs

--- src-player_client/poller.py
+++ src-player_client/poller.py
@@ -1,29 +1,42 @@
-import requests
-import time
-import polling
-import queue
+import asyncio
+import aiohttp
+import aiomysql
+import socket
 
 
-
-#https://github.com/justiniso/polling 
-def test(response):
-    global status_code
-    status_code = response.status_code
-    return response.status_code == 200
-
-my_queue = queue.Queue()
-
-result = polling.poll(lambda: requests.get('http://google.com'), 
-                      step=60, 
-                      poll_forever=True, 
-                      check_success=test,
-                      collect_value=my_queue)
+async def get_dns_status():
+    try:
+        ip = await asyncio.get_event_loop().getaddrinfo('www.google.com', None)
+        return ip[0][-1][0]
+    except socket.gaierror:
+        return "DNS Error"
 
 
+async def get_http_status():
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get('https://www.google.com') as response:
+                return response.status
+        except aiohttp.ClientConnectorError:
+            return "HTTP Error"
 
-while True:
-    if my_queue.empty():
-        print("empty {}".format(status_code))
-    else:
-        print("{} {}".format(my_queue.get()), status_code)
-    time.sleep(300)  # Sleep for 5 minutes
+
+async def execute_sql_query(query):
+    pool = await aiomysql.create_pool(host='localhost', port=3306,user='root', password='root',db='Juno')
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(query)
+            results = await cur.fetchall()
+            return results
+
+
+async def main():
+    dns_status = await get_dns_status()
+    http_status = await get_http_status()
+    #sql_query_results = await execute_sql_query('SELECT * FROM devices')
+    print(f"DNS Status: {dns_status}")
+    print(f"HTTP Status: {http_status}")
+    #print(f"SQL Query Results: {sql_query_results}")
+
+
+asyncio.run(main())
