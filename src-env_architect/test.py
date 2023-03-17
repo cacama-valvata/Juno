b05acecee6c20ec5eb728e22470e35ae4c2623d9
Arian Ghorbani <arighorbani@gmail.com>
2023-03-16 19:50:38 -0700

    Wrote some basic pseudocode for -2 implementation

--- src-env_architect/test.py
+++ src-env_architect/test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 import json
 file = open("./settings.json", "r")
-#print(json.dumps(filedata)) #test line
 parsed = json.load(file)
 
 scriptwrapper = open("./scriptwrapper.txt", 'w')
@@ -13,3 +12,26 @@ scriptwrapper.close()
 image = open("image.txt", 'w')
 image.write(parsed['image'] + "\n")
 image.close()
+
+# -2 iteration pseudocode
+# import mysql.connector
+# mydb = mysql.connector.connect(
+#    host="localhost",
+#    user="username",
+#    password="password",
+#    database="database"
+# )
+# mycursor = mydb.cursor()
+# mycursor.execute("SELECT scriptname, dirpath FROM scripts WHERE dirpath = $DIRPATH")
+# scripts = mycursor.fetchall()
+# #for x in scripts:
+# # print(x)
+#
+# mycursor.execute("SELECT imagename, dirpath FROM images WHERE dirpath = $DIRPATH")
+# images = mycursor.fetchall()
+# #for x in images:
+# # print(x)
+# 
+# for script in scripts, write the name of the script at dirpath to settings.json
+# for image in images, write the name of the script at dirpath to settings.json
+# 
