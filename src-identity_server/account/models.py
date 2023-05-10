27faa66d932eb803581b6d819debaa0fcca1fff5
Casey Colley <code@caseycolley.com>
2023-05-10 03:20:16 -0700

    more client-friendly form validation on add_devices modal

--- src-identity_server/account/models.py
+++ src-identity_server/account/models.py
@@ -24,7 +24,7 @@ class UserDevice (models.Model):
     keyname = models.CharField (max_length=200, null=False)
     ssh_prefix = models.CharField (max_length=64, null=False)
     ssh_pubkey = models.CharField (max_length=600, null=False, unique=True)
-    ssh_suffix = models.CharField (max_length=256, null=False)
+    ssh_suffix = models.CharField (max_length=256, null=True)
     last_seen = models.DateTimeField (null=True)
     default = models.BooleanField (default=False)
 
--- src-identity_server/account/views.py
+++ src-identity_server/account/views.py
@@ -4,6 +4,7 @@ from django.http import *
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.db import IntegrityError
 
 from .models import *
 from .forms import *
@@ -18,26 +19,36 @@ def DevicesView (request):
     return render (request, "profile/devices.html", {"devices": thisuser_devices, "form": AddDeviceForm()})
 
 def AddDevice (request):
-    if request.method != 'POST':
-        return HttpResponseBadRequest()
-    
-    form = AddDeviceForm (request.POST)
-    if not form.is_valid():
-        return HttpResponseBadRequest()
-    
-    keyname = form.cleaned_data['keyname']
-    ssh_key = form.cleaned_data['ssh_key'].strip().split(' ')
-    if len(ssh_key) != 3:
-        return HttpResponseBadRequest()
-    
-    ssh_prefix = ssh_key[0]
-    ssh_pubkey = ssh_key[1]
-    ssh_suffix = ssh_key[2]
+    if request.method == 'POST':
+        form = AddDeviceForm (request.POST)
+        if form.is_valid():
+            keyname = form.cleaned_data['keyname']
+            ssh_key = form.cleaned_data['ssh_key'].strip().split(' ')
 
-    new_device = UserDevice (user=request.user, keyname=keyname, ssh_prefix=ssh_prefix, ssh_pubkey=ssh_pubkey, ssh_suffix=ssh_suffix)
-    new_device.save()
+            # ssh-rsa AAAAA..... user@host
+            # user@host is actually an optional 'comment' in OpenSSH
+            if len (ssh_key) >= 2:
+                ssh_prefix = ssh_key[0]
+                ssh_pubkey = ssh_key[1]
+                ssh_suffix = ssh_key[2] if len(ssh_key) > 2 else "N/A"
 
-    return HttpResponseRedirect("/profile/devices/")
+                new_device = UserDevice (user=request.user, keyname=keyname, ssh_prefix=ssh_prefix, ssh_pubkey=ssh_pubkey, ssh_suffix=ssh_suffix)
+                # Prevent duplicate keys
+                try:
+                    new_device.save()
+                except IntegrityError:
+                    # TODO: better error display than this would require jquery and client-js
+                    form.errors["ssh_key"] = ["Duplicate SSH key. Generate a new one."]
+                else:
+                    return HttpResponseRedirect ('/profile/devices/')
+
+            else:
+                form.errors["ssh_key"] = ["SSH keys require at least a key type and public key data."]
+    else:
+        form = AddDeviceForm()
+    
+    thisuser_devices = UserDevice.objects.filter (user=request.user)
+    return render (request, "profile/devices.html", {"devices": thisuser_devices, "form": form})
 
 
 def ProfilePage (request):
