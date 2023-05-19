ed7ea6ef7fc41d6a25151365b7dc1b7bd7b57c0e
Casey Colley <code@caseycolley.com>
2023-05-18 17:28:23 -0700

    removed test data - was added for playerclient testing

--- src-identity_server/Juno_tables/show_fakeusers.sql
+++
@@ -1,8 +0,0 @@
--- Use Juno
-
-use Juno;
-
--- Show tables for users and devices; verifies the insertion worked
-
-select * from users;
-select keyname, ssh_prefix, ssh_suffix, userid, last_seen from devices;
